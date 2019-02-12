53df61d2fea2aed85c5293836f5da69774efbbaa
Caina C. Caldas <cainacc@hotmail.com>
2019-02-12 01:59:37 -0200

    added camera raycaster

---
+++ GitUnityExerc/Assets/camera/RayHit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        int layerMask = 0;
+        layerMask = ~layerMask; //hits everything
+        if (Physics.Raycast(ray , out hit , Mathf.Infinity , layerMask)) {
+            if (Input.GetMouseButtonDown(0)) {
+                print("character should move to here " + hit.point);
+            }
+        }
+    }
+}
---
+++ GitUnityExerc/Assets/camera/cameraArm.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraArm : MonoBehaviour
+{
+    public Transform target;
+    Vector3 arm;
+
+    void Start()
+    {
+        arm = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + arm;
+    }
+}
