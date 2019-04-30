ab27caf0723349b630a13601c90c7953abec5565
Diana Calheiro <leo@Mac-Diana.local>
2019-04-30 16:36:57 -0300

    comecamos unity

---
+++ GitUnityExerc/Assets/Luigi/LJogador.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LJogador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0,10,0);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(6,0,0);
+            print("apertou left");
+        }
+        
+    }
+}
