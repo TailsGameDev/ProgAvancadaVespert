248593558c9bacc2fc2e71af1f21fd48d0d0ad96
Caina C. Caldas <cainacc@hotmail.com>
2019-05-01 22:59:19 -0300

    esqueleto do sistema ae

---
+++ GitUnityExerc/Assets/AnimationSystem/AnimFSM.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Estados {
+    IDLE,
+    WALK
+}
+
+public class AnimFSM : MonoBehaviour
+{
+    Estados estadoAtual = Estados.IDLE;
+    Estados proximoEstado = Estados.WALK;
+    C_Animation c_Animation;
+
+    public Sprite[] idleSprites, walkSprites;
+
+    void Start()
+    {
+      c_Animation = GetComponent<C_Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animFSM(Input.GetKey(KeyCode.Space));
+    }
+
+    void animFSM(bool andando){
+
+      //FSM
+      switch(estadoAtual){
+        case Estados.IDLE:
+          if (andando){
+            c_Animation.sprites = walkSprites;
+            c_Animation.transicao = true;
+            proximoEstado = Estados.WALK;
+          }
+          break;
+        case Estados.WALK:
+          if (!andando){
+            c_Animation.sprites = idleSprites;
+            c_Animation.transicao = true;
+            proximoEstado = Estados.IDLE;
+          }
+          break;
+      }
+
+      //atualizacao
+      estadoAtual = proximoEstado;
+    }
+}
---
+++ GitUnityExerc/Assets/AnimationSystem/C_Animation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_Animation : MonoBehaviour
+{
+    // voce pode arrastar varios sprites de uma vez para setar esse vetor!!
+    public Sprite[] sprites;
+    public uint framesPorSegundo; // int positivo
+    SpriteRenderer sr;
+    public bool transicao;
+
+
+    void Start()
+    {
+      sr = GetComponent<SpriteRenderer>();
+      StartCoroutine(RunAnim());
+    }
+
+    // vai percorrendo o array Sprites e passando os frames
+    IEnumerator RunAnim (){
+      int  numeroDoFrame= 0;
+      float timer = 0;
+      float t = 1/framesPorSegundo;
+      while (true){
+        timer+= Time.deltaTime;
+        bool deuTempoMaximo = timer >= t;
+        //o if atualiza quando ha transicao de animacao ou no tempo maximo
+        if (transicao || deuTempoMaximo){
+          timer = 0; transicao = false;
+          sr.sprite = sprites[numeroDoFrame];
+          numeroDoFrame = (numeroDoFrame + 1) % sprites.Length;
+        }
+        yield return null;
+      }
+    }
+
+}
