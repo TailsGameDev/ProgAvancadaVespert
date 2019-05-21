08531fc723bf9502597164e26c3c7cdea1737ae7
Caina C. Caldas <cainacc@hotmail.com>
2019-05-21 17:05:13 -0300

    Personagem atira e inimigo toma dano

---
+++ GitUnityExerc/Assets/SpaceShooter/cDahDano.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cDahDano : MonoBehaviour
+{
+    public int dano = 1;
+
+    public virtual void BateuEmAlgo() {
+        Destroy(gameObject);
+    }
+}
---
+++ GitUnityExerc/Assets/SpaceShooter/cTomaDano.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cTomaDano : MonoBehaviour
+{
+    public int vida;
+
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D (Collider2D col) {
+        cDahDano tiro = col.GetComponent<cDahDano>();
+        if (tiro == null) return;
+
+        // se estiver aqui significa que bateu em um tiro.
+        vida -= tiro.dano;
+
+        tiro.BateuEmAlgo();
+
+        if (vida <= 0)
+            Destroy(gameObject);
+    }
+
+}
