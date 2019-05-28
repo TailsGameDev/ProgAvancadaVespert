6be7fe48469868535990eec9114c894489bd08be
Caina C. Caldas <cainacc@hotmail.com>
2019-05-28 14:54:39 -0300

    push rapidao camera lerp

---
+++ GitUnityExerc/Assets/SpaceShooter/LerpCam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpCam : MonoBehaviour
+{
+    public Transform CoisaPraSeguir;
+
+    private void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = Vector3.Lerp(transform.position, CoisaPraSeguir.position, 0.02f);
+        pos = new Vector3(pos.x , pos.y , -10);
+        transform.position = pos;
+    }
+
+
+    //nao esta sendo usada essa corrotina. Era soh um exemplo pro Franco
+    IEnumerator minhaCorrotina(float o) {
+        yield return new WaitForSeconds(o);
+        Destroy(gameObject);
+
+        float t = 0;
+        while (t < o) {
+            yield return null;
+            t += Time.deltaTime;
+        }
+    }
+}
---
+++ GitUnityExerc/Assets/lerpScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lerpScript : MonoBehaviour
+{
+    public Transform player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Vector3.Lerp(transform.position , player.position , 0.02f);
+        pos = new Vector3(pos.x , pos.y , -10);
+        transform.position = pos;
+    }
+}
