348d81fef632a2a6eb477c734002182e34c99870
Caina C. Caldas <cainacc@hotmail.com>
2019-06-11 14:27:52 -0300

    11/6 14:30

---
+++ GitUnityExerc/Assets/SpaceShooter/cSeDestroi.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cSeDestroi : MonoBehaviour
+{
+    public float tempoParaDestruicao;
+
+    void Start () {
+        Invoke("Destroise", tempoParaDestruicao);
+    }
+
+    void Destroise () {
+        Destroy(this.gameObject);
+    }
+}
